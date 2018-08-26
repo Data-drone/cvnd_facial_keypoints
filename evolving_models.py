3f8ca287d8f8debd0a7075f7e724d0b49f08ff52
Data-drone <bpl.law@gmail.com>
2018-08-26 16:25:21 +0000

    added Q2

---
+++ evolving_models.py
@@ -0,0 +1,207 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import seaborn as sns
+import logging
+
+
+def get_logger():
+    logger = logging.getLogger('train_script')
+    logger.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    return logger
+
+
+def train_net(n_epochs, network,  print_train = True):
+
+    # prepare the net for training
+    network.train()
+    results = []
+
+    for epoch in range(n_epochs):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+
+        # train on batches of data, assumes you already have train_loader
+        for batch_i, data in enumerate(train_loader):
+            # get the input images and their corresponding labels
+            images = data['image']
+            key_pts = data['keypoints']
+
+            # flatten pts
+            key_pts = key_pts.view(key_pts.size(0), -1)
+
+            # cuda flags
+            if torch.cuda.is_available():
+                run_images = images.type(torch.FloatTensor).to('cuda')
+                run_key_pts = key_pts.type(torch.FloatTensor).to('cuda')
+            else:
+                logger.debug('moving images to cpu for epoch {}'.format(epoch))
+                run_images = images.type(torch.FloatTensor)
+                run_key_pts = key_pts.type(torch.FloatTensor)
+
+            # forward pass to get outputs
+            output_pts = network(run_images)
+
+            # calculate the loss between predicted and target keypoints
+            loss = criterion(output_pts, run_key_pts)
+
+            # zero the parameter (weight) gradients
+            optimizer.zero_grad()
+
+            # backward pass to calculate the weight gradients
+            loss.backward()
+
+            # update the weights
+            optimizer.step()
+
+            # print loss statistics
+            running_loss += loss.item()
+            if batch_i % 10 == 9:    # print every 10 batches
+                if print_train:
+                    logger.debug('Epoch: {}, Batch: {}, Avg. Loss: {}'.format(epoch + 1, batch_i +1, running_loss /10))
+                    results.append([epoch + 1, batch_i +1, running_loss /10])
+                running_loss = 0.0
+
+    logger.info('Finished Training')
+    return results
+
+
+def analyse_results(network):
+    SmoothL1Loss = []
+    L1LossList = []
+    MSELossList = []
+
+    logger.debug('moving network to cpu')
+    infer_net = network.to('cpu')
+
+    SmoothL1 = nn.SmoothL1Loss()
+    L1Loss = nn.L1Loss()
+    MSELoss = nn.MSELoss()
+
+    for i, sample in enumerate(test_loader):
+        # get sample data: images and ground truth keypoints
+        #logger.debug('moving data to cpu')
+        images = sample['image'].type(torch.FloatTensor).to('cpu')
+        key_pts = sample['keypoints'].type(torch.FloatTensor).to('cpu')
+
+        # forward pass to get net output
+        output_pts = infer_net(images)
+
+        # reshape to batch_size x 68 x 2 pts
+        output_pts = output_pts.view(output_pts.size()[0], 68, -1)
+
+        SmoothL1num = SmoothL1(output_pts, key_pts)
+        SmoothL1Loss.append(SmoothL1num.data.numpy())
+
+        L1Lossnum = L1Loss(output_pts, key_pts)
+        L1LossList.append(L1Lossnum.data.numpy())
+
+        MSELossnum = MSELoss(output_pts, key_pts)
+        MSELossList.append(MSELossnum.data.numpy())
+
+    return SmoothL1Loss, L1LossList, MSELossList
+
+
+####################### Main Routine
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import itertools
+
+# the dataset we created in Notebook 1 is copied in the helper file `data_load.py`
+from data_load import FacialKeypointsDataset
+# the transforms we defined in Notebook 1 are in the helper file `data_load.py`
+from data_load import Rescale, RandomCrop, Normalize, ToTensor
+
+logger = get_logger()
+
+####################### Load Data ###################
+
+data_transform = transforms.Compose([Rescale(250),
+                                     RandomCrop(224),
+                                     Normalize(),
+                                     ToTensor()])
+
+# testing that you've defined a transform
+assert(data_transform is not None), 'Define a data_transform'
+
+######################## Transform Data #############
+
+# create the transformed dataset
+transformed_dataset = FacialKeypointsDataset(csv_file='data/training_frames_keypoints.csv',
+                                             root_dir='data/training/',
+                                             transform=data_transform)
+
+# Parameter
+batch_size_set = 30
+n_epochs = 30
+
+# Train Loader
+train_loader = DataLoader(transformed_dataset,
+                          batch_size=batch_size_set,
+                          shuffle=True,
+                          num_workers=0)
+
+# Test Loader
+# create the test dataset
+test_dataset = FacialKeypointsDataset(csv_file='data/test_frames_keypoints.csv',
+                                             root_dir='data/test/',
+                                             transform=data_transform)
+
+test_loader = DataLoader(test_dataset,
+                          batch_size=batch_size_set,
+                          shuffle=True,
+                          num_workers=0)
+
+######################### network to use ##############
+from models import Net, Net_wo_drop, Net_drop_more_layer, NaimNet, Net_drop
+
+######################### Train Network ###############
+
+#
+models = {'Vanilla CNN': Net(),
+          'Vanilla Net no dropouts': Net_wo_drop(),
+          'Vanilla net with dropouts': Net_drop(),
+          'Vanilla net with more dropouts': Net_drop_more_layer(),
+          'NaimishNet': NaimNet()}
+
+experiments = []
+for model in models.keys():
+
+    ########################## Make sure we have a fresh net each iteration
+    net = models[model]
+
+    if torch.cuda.is_available():
+        logger.debug('CUDA Training')
+        net.to('cuda')
+
+    logger.info('running with model: {}'.format(model))
+    criterion = nn.SmoothL1Loss()
+    optimizer = optim.Adam(net.parameters(), lr=0.001, amsgrad=True)
+
+    train_net(n_epochs, net, print_train = False)
+    SmoothL1Loss, L1LossList, MSELossList = analyse_results(net)
+
+    print(model)
+    sns.distplot(SmoothL1Loss, label = model)
+
+    print('Smooth L1 {}'.format(np.mean(SmoothL1Loss)))
+    print('L1 {}'.format(np.mean(L1LossList)))
+    print('SME L1 {}'.format(np.mean(MSELossList)))
+
+    experiments.append({'name': model,
+                        'Smooth-L1': SmoothL1Loss,
+                        'L1': L1LossList,
+                        'SME': MSELossList})
+
+    del net
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
--- models.py
+++ models.py
@@ -8,50 +8,196 @@ import torch.nn.init as I
 
 
 class Net(nn.Module):
+    # Based on https://arxiv.org/pdf/1511.04031.pdf
 
     def __init__(self):
         super(Net, self).__init__()
-        
-        ## TODO: Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (same width and height), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        
+
+        self.conv1 = nn.Conv2d(1, 16, 5, stride=1, padding=2)  # add padding to keep image size
+        self.batch1 = nn.BatchNorm2d(16, 0.9)
+        self.conv2 = nn.Conv2d(16, 48, 3, stride=1, padding=1)
+
+        self.mpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(48, 64, 3)
+
+        self.conv4 = nn.Conv2d(64, 64, 2)
+
+        self.relu = nn.ReLU(inplace=True)
+        # self.batchnorm = nn.BatchNorm2d(32)
+
+        # self.fc_lin1 = nn.Linear(640000, 1024)
+
+        self.fc_lin2 = nn.Linear(43264, 136)
+
+    def forward(self, x):
+
+        ## x = self.pool(F.relu(self.conv1(x)))
+        # x = self.relu(self.conv1(x))
+        x = self.relu(self.batch1(self.conv1(x)))
+        x = self.mpool(x)
+        x = self.mpool(self.relu(self.conv2(x)))
+
+        x = self.mpool(self.relu(self.conv3(x)))
+        x = self.relu(self.conv4(x))
+
+        x = x.view(x.size(0), -1)
+        x = F.dropout(x, training=self.training)
+        # x = self.fc_lin1(x)
+        x = self.fc_lin2(x)
+
+        return x
+
+
+class Net_wo_drop(nn.Module):
+    # Version of Net with no dropout layers
+
+    def __init__(self):
+        super(Net_wo_drop, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 16, 5, stride=1, padding=2)  # add padding to keep image size
+        self.conv2 = nn.Conv2d(16, 48, 3, stride=1, padding=1)
+        self.mpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(48, 64, 3)
+        self.conv4 = nn.Conv2d(64, 64, 2)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.fc_lin2 = nn.Linear(43264, 136)
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.mpool(x)
+        x = self.mpool(self.relu(self.conv2(x)))
+
+        x = self.mpool(self.relu(self.conv3(x)))
+        x = self.relu(self.conv4(x))
+
+        x = x.view(x.size(0), -1)
+        x = self.fc_lin2(x)
+
+        return x
+
+
+class Net_drop(nn.Module):
+    # version of Net with normalisation
+
+    def __init__(self):
+        super(Net_drop, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 16, 5, stride=1, padding=2)  # add padding to keep image size
+        self.batch1 = nn.BatchNorm2d(16, 0.9)
+        self.conv2 = nn.Conv2d(16, 48, 3, stride=1, padding=1)
+        self.batch2 = nn.BatchNorm2d(48, 0.9)
+        self.mpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv2d(48, 64, 3)
+
+        self.conv4 = nn.Conv2d(64, 64, 2)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.fc_lin2 = nn.Linear(43264, 136)
+
+    def forward(self, x):
+        x = self.relu(self.batch1(self.conv1(x)))
+        x = self.mpool(x)
+        x = self.mpool(self.relu(self.batch2(self.conv2(x))))
+
+        x = self.mpool(self.relu(self.conv3(x)))
+        x = self.relu(self.conv4(x))
+
+        x = x.view(x.size(0), -1)
+        x = F.dropout(x, training=self.training)
+        x = self.fc_lin2(x)
+
+        return x
+
+
+class Net_drop_more_layer(nn.Module):
+    # version of net with more layers and normalisation
+
+    def __init__(self):
+        super(Net_drop_more_layer, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 16, 5, stride=1, padding=2)  # add padding to keep image size
+        self.batch1 = nn.BatchNorm2d(16, 0.9)
+
+        self.conv2 = nn.Conv2d(16, 48, 3, stride=1, padding=1)
+        self.batch2 = nn.BatchNorm2d(48, 0.9)
+
+        self.conv3 = nn.Conv2d(48, 64, 3)
+        self.batch3 = nn.BatchNorm2d(64, 0.9)
+
+        self.conv4 = nn.Conv2d(64, 96, 2)
+        self.batch4 = nn.BatchNorm2d(96, 0.9)
+
+        self.conv5 = nn.Conv2d(96, 96, 2)
+
+        self.mpool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.fc_lin2 = nn.Linear(13824, 136)
+
+    def forward(self, x):
+        x = self.mpool(self.relu(self.batch1(self.conv1(x))))
+        x = self.mpool(self.relu(self.batch2(self.conv2(x))))
+        x = self.mpool(self.relu(self.batch3(self.conv3(x))))
+        x = self.mpool(self.relu(self.batch4(self.conv4(x))))
+
+        x = self.relu(self.conv5(x))
+
+        x = x.view(x.size(0), -1)
+        x = F.dropout(x, training=self.training)
+        x = self.fc_lin2(x)
+        return x
+
+
+class NaimNet(nn.Module):
+    # based on https://arxiv.org/pdf/1710.00977.pdf
+
+    def __init__(self):
+        super(NaimNet, self).__init__()
+
+
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        self.mpool = nn.MaxPool2d(kernel_size=2)
-        
-        #self.conv3 = nn.Conv2d(64, 128, 5)
-        #self.conv4 = nn.Conv2d(128, 256, 5)
-        
-        self.relu = nn.ReLU(inplace=True)
-        #self.batchnorm = nn.BatchNorm2d(32)
-        
-        #self.fc_lin1 = nn.Linear(640000, 1024)
-        
-        self.fc_lin2 = nn.Linear(746496, 136)
-        
-        
+        self.conv1 = nn.Conv2d(1, 32, 4, stride=1, padding=0)  # add padding to keep image size
+        self.drop1 = nn.Dropout(p=0.1)
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=1, padding=0)
+        self.drop2 = nn.Dropout(p=0.2)
+        self.conv3 = nn.Conv2d(64, 128, 2)
+        self.drop3 = nn.Dropout(p=0.3)
+        self.conv4 = nn.Conv2d(128, 256, 1)
+        self.drop4 = nn.Dropout(p=0.4)
+
+        self.mpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.elu = nn.ELU(inplace=True)
+
+        self.drop5 = nn.Dropout(p=0.5)
+        self.drop6 = nn.Dropout(p=0.6)
+
+        self.fc_lin1 = nn.Linear(43264, 1000)
+        self.fc_lin2 = nn.Linear(1000, 1000)
+
+        self.fc_lin3 = nn.Linear(1000, 136)
+
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
 
-        
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        x = self.relu(self.conv1(x))
-        x = self.mpool(self.relu(self.conv2(x)))
-        
-        #x = self.relu(self.conv3(x))
-        #x = self.mpool(self.relu(self.conv4(x)))
-        
+        # x = self.relu(self.conv1(x))
+        x = self.drop1(self.mpool(self.elu(self.conv1(x))))
+        x = self.drop2(self.mpool(self.elu(self.conv2(x))))
+        x = self.drop3(self.mpool(self.elu(self.conv3(x))))
+        x = self.drop4(self.mpool(self.elu(self.conv4(x))))
+
         x = x.view(x.size(0), -1)
-        #x = F.dropout(x,inplace=True)
-        #x = self.fc_lin1(x)
-        x = self.fc_lin2(x)
+
+        # x = F.dropout(x,training=self.training)
+        x = self.drop5(self.elu(self.fc_lin1(x)))
+        x = self.drop6(self.elu(self.fc_lin2(x)))
+        x = self.fc_lin3(x)
         # a modified x, having gone through all the layers of your model, should be returned
         return x
